152b8d8b7a6df26982c45e75eae6dbceebc14e0a
Ben G <bensgar92@gmail.com>
2021-01-21 22:06:16 -0700

    sign up page utilizing express, mongoose, mongoDB and bcrypt is fully functional

--- signupfront/signupbackend/routes/routes.js
+++ signupfront/signupbackend/routes/routes.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router()
 const signUpTemplateCopy = require('../models/SignUpModels')
+const bcrypt = require('bcrypt')
 
 //this is a signup application so all we are expecting is a post request - only sending post request to mongoDB
-router.post('/signup', (request, response) => {
+router.post('/signup', async(request, response) => {
+
+    //first line for bcrypt is to generate a salt for the password
+    const saltPassword = await bcrypt.genSalt(10)
+    //second line is to salt the password that has been hashed and then pass the variable down below
+    const securePassword = await bcrypt.hash(request.body.password, saltPassword)
+
     const signedUpUser = new signUpTemplateCopy({
         fullName:request.body.fullName,
         username:request.body.username,
         email:request.body.email,
-        password:request.body.password
+        password:securePassword
     })
     signedUpUser.save()
     .then(data =>{
